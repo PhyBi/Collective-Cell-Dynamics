4f05ce0163d99848a371f2126969e4454df57c79
Somajit <dey.somajit@gmail.com>
2022-12-12 19:34:47 +0530

    Solely bead-based neighbor (linked) list. Simpler interaction() subroutine

--- src/mod_grid_linked_list.f90
+++ src/mod_grid_linked_list.f90
@@ -7,8 +7,7 @@ module grid_linked_list
        double precision, protected :: celli
 	   integer, protected:: gridmapsiz
 	   integer, dimension(:), allocatable:: gridmap
-	   integer, dimension(:), allocatable:: headcell
-       integer, dimension(:,:), allocatable:: headbead,listcell,listbead
+       integer, dimension(:), allocatable :: bead_nl_head, bead_nl_body ! bead-only neighborlist: head and body
        private :: grid_index
        
     contains
@@ -31,7 +30,7 @@ module grid_linked_list
     ncell=w*w
     gridmapsiz=4*ncell
     
-    allocate(gridmap(gridmapsiz), headcell(ncell), headbead(ncell,m),listcell(ncell,m),listbead(m,n), stat=alloc_stat)
+    allocate(gridmap(gridmapsiz), bead_nl_head(ncell), bead_nl_body(m*n), stat=alloc_stat)
         
      if(alloc_stat /= 0) error stop 'Problem while allocating grid_linked_list'
     
@@ -54,43 +53,30 @@ module grid_linked_list
 
 	!!*** Subroutine to make linked lists & head of chain arrays ***!!
 	subroutine links()
-	integer:: icell,l,i
-    integer, dimension(size(headcell)):: headcell_dummy
-    double precision, dimension(size(x,1),size(x,2)):: x3,y3
+	integer:: icell,l,i, bead_index
+    double precision :: x3,y3
 	
    	!! Zero Head Of Chain Array & List Array !!
-
-
-    do icell=1,ncell
-    headcell(icell)=0 !! head of chain array for the rings in a cell
-	headcell_dummy(icell)=0
-	listcell(icell,:)=0 !! List array for the rings in a cell
-	headbead(icell,:)=0 !! head of chain array for the beads in a ring in the cell
-    end do
+    bead_nl_head = 0
+    bead_nl_body = 0
 
 	!! Sort All Beads !!
 
     do l=1,m	 !! Loop over rings
-		listbead(l,:)=0
         do i=1,n !! Loop over beads
 
-		        x3(l,i) = x(l,i) - box*floor(x(l,i)/box)
-                        y3(l,i) = y(l,i) - box*floor(y(l,i)/box)
+		    x3 = x(l,i) - box*floor(x(l,i)/box)
+            y3 = y(l,i) - box*floor(y(l,i)/box)
 
 
-			icell = 1 + int(x3(l,i) * celli) + int(y3(l,i) * celli) * w  !! determining the cell index for a particular bead
+			icell = 1 + int(x3 * celli) + int(y3 * celli) * w  ! determining the grid index for a particular bead
 
 			
-			
-			listcell(icell,l)     = headcell(icell)     !! The next lower ring index in that cell(icell)
-			listbead(l,i)         = headbead(icell,l)   !! The next lower bead index of a part of a ring(l) in a cell(icell) 
-			headbead(icell,l)     = i	            !! Highest bead index in the part of the ring(l) in a cell(icell)
-			headcell_dummy(icell) = l
+			bead_index = (l-1)*n+i ! Global serial of the bead : [1,mn]
 
+			bead_nl_body(bead_index) = bead_nl_head(icell)
+			bead_nl_head(icell) = bead_index
 		end do	
-
-		headcell = headcell_dummy                   !! Highest ring index in a cell(icell) 
-
 	end do
 	end subroutine links
 
