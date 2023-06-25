6eddd8aab5323734cf12d75b35b9f3a8efdf6a0b
Somajit <dey.somajit@gmail.com>
2023-06-25 20:23:53 +0530

    new initialization with energy/overlap minimization

--- src/mod_init.f90
+++ src/mod_init.f90
@@ -6,54 +6,119 @@ contains
 
 !!! Subroutine for random initial configurations
 subroutine initial
-double precision:: radius   ! Initial/seed cell radius
-double precision:: acell,bcell,dr,mindist,dx,dy, theta1
-double precision :: m_tmp,mx_tmp,my_tmp, mx_cell,my_cell
-integer:: l,kcell,i,fail_count
-real:: rands(2)
-double precision, dimension(size(x,dim=2)) :: xcell, ycell ! centre coordinates of any circular cell/ring
+double precision:: radius_l0   ! initial/seed cell radius
+double precision:: mindist, mindist2 ! initial separation between cell centres to ensure non-overlap
+double precision:: radius_eq ! equilibrium radius, achieved by force balance between spring and pressure
+double precision:: dist_eq ! equilibrium distance (centre-centre)
 
-radius = 0.5d0*l0/dsin(pi/n) ! circumcirle of a regular n-gon with side l0
-dr = rc_rep ! initial separation between cell peripheries
-mindist = 2*radius+dr
-box = dsqrt(m*pi*mindist*mindist/4) ! box should accommodate m circumcircles with dr separation
+character(len=:), allocatable :: argument
+integer :: arglen
+
+real:: rands(2)
+double precision:: xcell_this,ycell_this,dx,dy
+double precision, dimension(size(x,dim=2)) :: xcell, ycell ! centre coordinates of any circular cell/ring
+double precision, dimension(size(x,dim=2)) :: xdisp, ydisp ! displacements during energy min
+integer:: this,other,iter_count,fail_count
+
+! Max trials while seeding and iterations during energy minimization
+integer, parameter:: max_trials_seed=1000, max_iters_emin=20000
+
+! For stability of energy minimization, number of cycles needed for overlap elimination of any pair
+integer, parameter:: cycles_pair_nonoverlap=100
+
+logical :: no_overlap_found
+
+integer:: i,l
+double precision :: angle,m_tmp,mx_tmp,my_tmp, mx_cell,my_cell
+
+! Seed cell properties
+radius_l0 = 0.5d0*l0/dsin(pi/n) ! circumcirle of a regular n-gon with side l0
+mindist = 2*radius_l0+rc_rep ! rc_rep is the minimum distance between cell peripheries
+mindist2 = mindist*mindist
+
+! If box length is provided by user, use that, estimate otherwise
+! In order to estimate, use the zero/negative stress condition whereby box can accommodate all equilibrium
+! cells without steric overlap
+call cmd_line_opt('--box', length=arglen)
+allocate(character(len=arglen) :: argument)
+call cmd_line_opt('--box', argument)
+if(argument /= '') then
+    read(argument,*)box
+else
+    radius_eq = (2*k*dtan(pi/n)*radius_l0) / (2*k*dtan(pi/n)-p)
+    if(radius_eq < 0.d0) error stop &
+        'Spring-Pressure force balance non-existent. Cannot estimate boxlength. Please provide with --box='
+    dist_eq = 2*radius_eq+rc_rep
+    box = dsqrt(m*pi*dist_eq*dist_eq/4) ! box should accommodate m equilibrium cells
+endif
+deallocate(argument)
 
 ! Seeding the first cell centre at origin
 xcell(1) = 0.d0
 ycell(1) = 0.d0
 
-! Find a suitable centre for each circular cell by random trial and error
-seed_cell_centres: do l=2,m
+! Find non-overlapping centres for circular cells by random trial and error
+! In case overlap is unavoidable even after so many trials, accept it
+seed_cell_centres: do this=2,m
     fail_count=0
 
     trial: do
         fail_count = fail_count+1 
-        if(fail_count > 1000) then
-            ! Too many failed seed_cell_centress means box is too small to accomodate yet another cell
-            ! Let's increase the box size
-            box = box*1.1d0
-            fail_count = 0
-        end if 
+        if(fail_count > max_trials_seed) exit trial
         
         CALL RANDOM_NUMBER(rands)
-        acell = box*rands(1)
-        bcell = box*rands(2)
+        xcell_this = box*rands(1)
+        ycell_this = box*rands(2)
 
-        distance_from_other_cells: do kcell=1,l-1
-            dx = acell-xcell(kcell)
-            dy = bcell-ycell(kcell)
+        distance_from_other_cells: do other=1,this-1
+            dx = xcell_this-xcell(other)
+            dy = ycell_this-ycell(other)
             dx = dx - box*nint(dx/box)
             dy = dy - box*nint(dy/box)
-            if ((dx*dx + dy*dy).lt.(mindist*mindist)) cycle trial
+            if ((dx*dx + dy*dy).lt.mindist2) cycle trial
         end do distance_from_other_cells
-
-        xcell(l) = acell
-        ycell(l) = bcell
-
         exit trial
     end do trial
 
+    xcell(this) = xcell_this
+    ycell(this) = ycell_this
 end do seed_cell_centres
+
+! Energy/overlap minimization
+iter_count = 0 ! initial count of iterations
+energy_min: do
+    no_overlap_found = .true.
+    if(iter_count > max_iters_emin) error stop 'Took too many cycles for energy/overlap minimization'
+
+    ! Loop over all pairs
+    do this=1,m-1
+        xcell_this = xcell(this)
+        ycell_this = ycell(this)
+        do other=this+1,m
+            dx = xcell_this-xcell(other)
+            dy = ycell_this-ycell(other)
+            dx = dx - box*nint(dx/box)
+            dy = dy - box*nint(dy/box)
+
+            ! Detect overlap
+            if ((dx*dx + dy*dy).lt.mindist2) then
+                no_overlap_found = .false.
+                xdisp(this) = dx/cycles_pair_nonoverlap
+                xdisp(other) = -xdisp(this)
+                ydisp(this) = dy/cycles_pair_nonoverlap
+                ydisp(other) = -ydisp(this)
+            endif
+        end do
+    end do
+    
+    if (no_overlap_found) exit energy_min
+    
+    ! Move cell centres towards non-overlap
+    xcell = xcell + xdisp
+    ycell = ycell + ydisp
+    
+    iter_count = iter_count + 1
+end do energy_min
 
 ! Construct circular cells from the seeded centres
 ! Also initialize the motility unit vectors randomly for each cell
@@ -65,9 +130,9 @@ do l=1,m
     mx_cell = mx_tmp/m_tmp
     my_cell = my_tmp/m_tmp
     do i=1,n        
-        theta1 = (i-1)*2.0d0*pi/n
-        x(i,l) = radius*dcos(theta1) + xcell(l)
-        y(i,l) = radius*dsin(theta1) + ycell(l)
+        angle = (i-1)*2.0d0*pi/n
+        x(i,l) = radius_l0*dcos(angle) + xcell(l)
+        y(i,l) = radius_l0*dsin(angle) + ycell(l)
         mx(i,l)= mx_cell
         my(i,l)= my_cell
      end do
